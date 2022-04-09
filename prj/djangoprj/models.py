3b10e195cdc949c4a0434edd23f03ea94828aeb6
LlamaUnicorn <holidaynight@gmail.com>
2022-04-09 09:03:44 +0300

    Initial commit

---
+++ prj/djangoprj/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Category(models.Model):
+# verbose_name='Название'
+    name = models.CharField(max_length=30, unique=True, verbose_name='Название')
+# blank позволяет сохранять пустые поля, null разрешает Null
+# editable=False - запрещает редактировать в админке
+# help_text - описание для админки
+# verbose_name='Описание'
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=32, verbose_name='Название')
+    price = models.PositiveSmallIntegerField(verbose_name='Цена')
+    # on_delete=protect запрещает удаление
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
